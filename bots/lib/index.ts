22f2158f4d43204739c1dbdee3469f662142c84a
scm2217 <smendonc@gmail.com>
2020-02-11 12:24:06 -0800

    index for easy importing of lib

---
+++ bots/lib/index.ts
@@ -0,0 +1,10 @@
+export { State } from "./state";
+export { ActionState } from "./ActionStates/actionState";
+export { FailureAction } from "./ActionStates/failureAState";
+export { SuccessAction } from "./ActionStates/successAState";
+export { BehaviorState } from "./BehaviorStates/behaviorState";
+export { FailureBehavior } from "./BehaviorStates/failureBState";
+export { SuccessBehavior } from "./BehaviorStates/successBState";
+export { Strategy } from "./Strategy/strategy";
+export { KnowledgeBase } from "./KnowledgeBase/KnowledgeBase";
+export { RoomMap } from "./KnowledgeBase/RoomMap";
