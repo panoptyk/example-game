56db78caa57c97128130bf44c48527391449c3f7
Mitchell Miller <mitchkm12@gmail.com>
2020-02-10 22:15:32 -0800

    remove requirement that an initial actionsState is passed from constructor

--- bots/lib/BehaviorStates/behaviorState.ts
+++ bots/lib/BehaviorStates/behaviorState.ts
@@ -5,11 +5,6 @@ export abstract class BehaviorState extends State {
 
   public currentActionState: ActionState;
 
-  constructor (initialState: ActionState, nextState: () => BehaviorState = undefined) {
-    super(nextState);
-    this.currentActionState = initialState;
-  }
-
   public async act () {
       this.currentActionState = await this.currentActionState.tick();
   }
