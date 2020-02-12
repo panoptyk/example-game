0c935a833c8fb30ca9b07bd0ce9f4a6ada9dcc66
jthpotz <jthpotz@gmail.com>
2020-02-12 09:46:46 -0800

    Adds Action FSM for Idle Behavior

--- bots/john_bots/MapAndMove/Behaviors/idleBehavior.ts
+++ bots/john_bots/MapAndMove/Behaviors/idleBehavior.ts
@@ -1,6 +1,16 @@
-import { BehaviorState } from "../../../lib";
+import { BehaviorState, ActionState } from "../../../lib";
+import { IdleAction } from "../Actions/idleAction";
 
 export class IdleBehavior extends BehaviorState {
+  constructor (nextState: () => BehaviorState) {
+    super (nextState);
+    this.currentActionState = new IdleAction (IdleBehavior.idleActionTransition);
+  }
+
+  public static idleActionTransition (this: IdleAction): ActionState {
+    return this;
+  }
+
   public nextState(): BehaviorState {
     return undefined;
   }
