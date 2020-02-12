f4728b26280a935f56ff74885437135c50e52efa
jthpotz <jthpotz@gmail.com>
2020-02-12 14:23:19 -0800

    Adds action FSM to move behavior

--- bots/john_bots/MapAndMove/Behaviors/moveBehavior.ts
+++ bots/john_bots/MapAndMove/Behaviors/moveBehavior.ts
@@ -1,6 +1,7 @@
-import { BehaviorState } from "../../../lib";
-import { Room } from "panoptyk-engine";
+import { BehaviorState, SuccessAction, ActionState } from "../../../lib";
+import { Room } from "panoptyk-engine/dist/client";
 import { TalkBehavior } from "./talkBehavior";
+import { MoveAction } from "../Actions/moveAction";
 
 export class MoveBehavior extends BehaviorState {
   public static destination: Room;
@@ -8,6 +9,18 @@ export class MoveBehavior extends BehaviorState {
   constructor(nextState: () => BehaviorState) {
     super(nextState);
     MoveBehavior.destination = TalkBehavior.useRoom();
+  }
+
+  public static moveActionTransition(this: MoveAction): ActionState {
+    if (
+      this.isMoveCompleted &&
+      this.moveDestination === MoveBehavior.destination
+    ) {
+      return SuccessAction.instance;
+    } else if (this.isMoveCompleted) {
+      return new MoveAction(MoveBehavior.moveActionTransition, undefined); // TODO: KnowledgeBase pathfinding tie in here
+    }
+    return this;
   }
 
   public nextState(): BehaviorState {
