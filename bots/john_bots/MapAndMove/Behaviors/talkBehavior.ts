99f88a7ccee5c21e2c45dcf5b20f54bf966e4604
jthpotz <jthpotz@gmail.com>
2020-02-15 00:51:01 -0800

    Creates action FSM for talk behavior

--- bots/john_bots/MapAndMove/Behaviors/talkBehavior.ts
+++ bots/john_bots/MapAndMove/Behaviors/talkBehavior.ts
@@ -1,17 +1,31 @@
-import { BehaviorState } from "../../../lib";
-import { Room, Agent } from "panoptyk-engine/dist/client";
+import {
+  BehaviorState,
+  SuccessAction,
+  KnowledgeBase,
+  FailureAction
+} from "../../../lib";
+import { Room, Agent, ClientAPI } from "panoptyk-engine/dist/client";
+import { AcceptConversationAction } from "../Actions/acceptConversationAction";
+import { CheckQuestionForRoomAction } from "../Actions/checkQuestionForRoomAction";
+import { EndConversationAction } from "../Actions/endConversationAction";
 
 export class TalkBehavior extends BehaviorState {
   private static room: Room = undefined;
   private static agent: Agent;
 
   constructor(nextState: () => BehaviorState) {
-    super (nextState);
+    super(nextState);
+    this.currentActionState = new AcceptConversationAction(
+      TalkBehavior.acceptConversationActionTransition
+    );
   }
-
 
   public static get receivedRoom(): Room {
     return TalkBehavior.receivedRoom;
+  }
+
+  public static get agentToTalkTo(): Agent {
+    return TalkBehavior.agent;
   }
 
   public static useRoom(): Room {
@@ -26,12 +40,43 @@ export class TalkBehavior extends BehaviorState {
     }
   }
 
-  public static get agentToTalkTo(): Agent {
-    return TalkBehavior.agent;
+  public static assignAgentToTalkTo(agent: Agent) {
+    TalkBehavior.agent = agent;
   }
 
-  public static assignAgentToTalkTo (agent: Agent) {
-    TalkBehavior.agent = agent;
+  public static acceptConversationActionTransition(
+    this: AcceptConversationAction
+  ) {
+    if (this.isConversationAccepted) {
+      return new CheckQuestionForRoomAction(
+        TalkBehavior.checkQuestionForRoomActionTransition
+      );
+    }
+    if (!KnowledgeBase.instance.isConversationRequested) {
+      return FailureAction.instance;
+    }
+    return this;
+  }
+
+  public static checkQuestionForRoomActionTransition(
+    this: CheckQuestionForRoomAction
+  ) {
+    if (this.isRoomFound) {
+      if (ClientAPI.playerAgent.conversation !== undefined) {
+        return new EndConversationAction(
+          TalkBehavior.endConversationActionTransition
+        );
+      }
+      return SuccessAction.instance;
+    }
+    return this;
+  }
+
+  public static endConversationActionTransition(this: EndConversationAction) {
+    if (this.isConversationLeft) {
+      return SuccessAction.instance;
+    }
+    return this;
   }
 
   public nextState(): BehaviorState {
