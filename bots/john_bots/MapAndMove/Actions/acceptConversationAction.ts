f10d48ac13f613747fd7a203447ef312acfcb5a7
jthpotz <jthpotz@gmail.com>
2020-02-15 00:40:09 -0800

    Adds logging and makes minor formatting changes

--- bots/john_bots/MapAndMove/Actions/acceptConversationAction.ts
+++ bots/john_bots/MapAndMove/Actions/acceptConversationAction.ts
@@ -1,15 +1,27 @@
 import { ActionState } from "../../../lib";
-import { ClientAPI } from "panoptyk-engine/dist/client";
+import { ClientAPI, ValidationResult } from "panoptyk-engine/dist/client";
 import { TalkBehavior } from "../Behaviors/talkBehavior";
 
 export class AcceptConversationAction extends ActionState {
+  private conversationAccepted = false;
 
-  constructor (nextState: () => ActionState) {
-    super (nextState);
+  public get isConversationAccepted(): boolean {
+    return this.conversationAccepted;
+  }
+
+  constructor(nextState: () => ActionState) {
+    super(nextState);
   }
 
   public async act() {
-    await ClientAPI.acceptConversation (TalkBehavior.agentToTalkTo);
+    await ClientAPI.acceptConversation(TalkBehavior.agentToTalkTo)
+      .catch((res: ValidationResult) => {
+        console.log(res.message);
+      })
+      .then(() => {
+        console.log("Accepted conversation with " + TalkBehavior.agentToTalkTo);
+        this.conversationAccepted = true;
+      });
   }
 
   public nextState(): ActionState {
