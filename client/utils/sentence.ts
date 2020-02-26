1d0bab2fd995c8702e98f389960c21fc942b8edc
scm2217 <smendonc@gmail.com>
2020-02-25 17:56:06 -0800

    new info and actions

--- client/utils/sentence.ts
+++ client/utils/sentence.ts
@@ -20,14 +20,16 @@ class Sentence {
   }
   public static fromInfo(info: Info): Sentence.Block[] {
     let blocks: Sentence.Block[] = undefined;
-    if (info.isQuery()) {
-      blocks = Sentence.createQuestion(info);
-    } else if (info.isCommand()) {
-      blocks = Sentence.createCommand(info);
-    } else if (info.isMasked()) {
-      blocks = Sentence.createMaskedFact(info);
-    } else {
-      blocks = Sentence.createFact(info);
+    if (info) {
+      if (info.isQuery()) {
+        blocks = Sentence.createQuestion(info);
+      } else if (info.isCommand()) {
+        blocks = Sentence.createCommand(info);
+      } else if (info.isMasked()) {
+        blocks = Sentence.createMaskedFact(info);
+      } else {
+        blocks = Sentence.createFact(info);
+      }
     }
     return blocks;
   }
@@ -71,19 +73,19 @@ class Sentence {
         const val = k.replace(/\d/, "");
         switch (val) {
           case "agent":
-            terms[k] = {agentName: fill};
+            terms[k] = { agentName: fill };
             break;
           case "loc":
-            terms[k] = {roomName: fill};
+            terms[k] = { roomName: fill };
             break;
           case "item":
-            terms[k] = {itemName: fill};
+            terms[k] = { itemName: fill };
             break;
           case "info":
-            terms[k] = {id: fill};
+            terms[k] = { id: fill };
             break;
           case "faction":
-            terms[k] = {factionName: fill};
+            terms[k] = { factionName: fill };
             break;
           default:
             break;
@@ -95,7 +97,6 @@ class Sentence {
     return terms;
   }
 
-
   /**
    * This is a really bad way to make these sentences...
    */
@@ -103,12 +104,15 @@ class Sentence {
     const arr = [];
     let agentName = terms.agent
       ? terms.agent.agentName
-      : terms.agent1.agentName;
+      : terms.agent1
+      ? terms.agent1.agentName
+      : "";
     if (agentName === ClientAPI.playerAgent.agentName) agentName = "You";
-    if (terms.agent2 &&
+    if (
+      terms.agent2 &&
       terms.agent2.agentName === ClientAPI.playerAgent.agentName
     ) {
-      terms.agent2 = {agentName: "You"};
+      terms.agent2 = { agentName: "You" };
     }
     arr.push({
       type: Sentence.BlockType.AGENT,
@@ -385,6 +389,38 @@ class Sentence {
         arr.push({
           type: Sentence.BlockType.NONE,
           text: "at "
+        });
+        arr.push({
+          type: Sentence.BlockType.ROOM,
+          text: terms.loc.roomName + " "
+        });
+        break;
+      case Info.ACTIONS.POSSESS.name:
+        arr.push({
+          type: Sentence.BlockType.ACTION,
+          text: "claimed to have possession of "
+        });
+        arr.push({
+          type: Sentence.BlockType.ITEM,
+          text: terms.item + " "
+        });
+        arr.push({
+          type: Sentence.BlockType.NONE,
+          text: "at "
+        });
+        arr.push({
+          type: Sentence.BlockType.ROOM,
+          text: terms.loc.roomName + " "
+        });
+        break;
+      case Info.ACTIONS.LOCATED_IN.name:
+        arr.push({
+          type: Sentence.BlockType.ITEM,
+          text: terms.item + " "
+        });
+        arr.push({
+          type: Sentence.BlockType.NONE,
+          text: " present at "
         });
         arr.push({
           type: Sentence.BlockType.ROOM,
