0fdfe58b288cdcaaec2c5ec176f87f3de45a114a
scm2217 <smendonc@gmail.com>
2020-02-27 19:13:00 -0800

    easier to read quests

--- client/ui/components/questEntry.vue
+++ client/ui/components/questEntry.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="quest-entry">
-      {{status}} <br> {{taskDescription}} <br>
-      <span v-for="b in sentence" v-bind:key="b.text" v-bind:class="b.type">
-        {{ b.text }}
-      </span>
+    {{ status }} <br />
+    {{ taskDescription }} <br />
+    <span v-for="b in sentence" v-bind:key="b.text" v-bind:class="b.type">
+      {{ b.text }}
+    </span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import { Quest } from "panoptyk-engine/dist/client";
+import { Quest, Info, ClientAPI } from "panoptyk-engine/dist/client";
 import Sentence from "../../utils/sentence";
 
-@Component({
-  
-})
+@Component({})
 export default class QuestEntry extends Vue {
   @Prop({ default: {} }) quest: Quest;
   get status() {
-    if(this.quest) {
+    if (this.quest) {
       return "Status: " + this.quest.status + "\n";
     }
   }
   get taskDescription() {
     if (this.quest) {
-      let sentence = "Description: " + this.quest.type + " quest assigned by " + this.quest.giver;
+      let sentence =
+        "Description: " +
+        this.quest.type +
+        " quest assigned by " +
+        this.quest.giver;
       switch (this.quest.type) {
         case "question":
           sentence += " with the mission to resolve the following mystery: ";
@@ -36,10 +39,85 @@ export default class QuestEntry extends Vue {
     }
   }
   get sentence() {
+    const terms = this.quest.task.getTerms();
+    const taskTxt = [];
+    switch (this.quest.type) {
+      case "command":
+        switch (this.quest.task.action) {
+          case Info.ACTIONS.GAVE.name:
+            taskTxt.push({
+              type: Sentence.BlockType.ACTION,
+              text: "Acquire and give "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.ITEM,
+              text: terms.item + " "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.NONE,
+              text: "to "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.AGENT,
+              text: terms.agent2.agentName + " "
+            });
+            return taskTxt;
+          case Info.ACTIONS.DROP.name:
+            taskTxt.push({
+              type: Sentence.BlockType.ACTION,
+              text: "Drop "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.ITEM,
+              text: terms.item + " "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.NONE,
+              text: "in "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.ROOM,
+              text: terms.loc
+            });
+            return taskTxt;
+          case Info.ACTIONS.TOLD.name:
+            taskTxt.push({
+              type: Sentence.BlockType.ACTION,
+              text: "Tell "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.AGENT,
+              text: terms.agent2 + " "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.NONE,
+              text: "about "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.INFO,
+              text: terms.info
+            });
+            return taskTxt;
+          case Info.ACTIONS.CONVERSE.name:
+            const targetAgent =
+              terms.agent1 === ClientAPI.playerAgent
+                ? terms.agent2
+                : terms.agent1;
+
+            taskTxt.push({
+              type: Sentence.BlockType.ACTION,
+              text: "Have a conversation with "
+            });
+            taskTxt.push({
+              type: Sentence.BlockType.AGENT,
+              text: terms.agent2 + " "
+            });
+            return taskTxt;
+        }
+    }
     return Sentence.fromInfo(this.quest.task);
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
