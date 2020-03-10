7c179c7b0e80b2d6b6adfe6e0354dd388224cad8
Mitchell Miller <mitchkm12@gmail.com>
2020-03-10 00:56:50 -0700

    item spawning

--- server/exampleServer.ts
+++ server/exampleServer.ts
@@ -1,6 +1,33 @@
 import app from "./app";
-import { Server } from "panoptyk-engine";
+import { Server, Room, Item, Controller } from "panoptyk-engine";
+import { Spawner } from "./spawner";
 
 const PanoptykServer = new Server(app);
 
 PanoptykServer.start();
+
+const spawnData: { roomID; itemID; max; rate }[] = [
+  {
+    roomID: 1,
+    itemID: 1,
+    max: 2,
+    rate: 1
+  }
+];
+
+const spawner = new Spawner();
+spawnData.forEach(data => {
+  spawner.addNewSpawn(
+    Room.getByID(data.roomID),
+    Item.getByID(data.itemID),
+    data.max,
+    data.rate
+  );
+});
+
+const spawnItems = function() {
+  spawner.checkSpawns(150);
+  // tslint:disable-next-line: ban
+  setTimeout(spawnItems, 150);
+};
+spawnItems();
---
+++ server/spawner.ts
@@ -0,0 +1,54 @@
+import { Room, Item, Controller } from "panoptyk-engine";
+
+class Spawner {
+  private _spawns: Spawner.SpawnData[];
+  constructor() {
+    this._spawns = [];
+  }
+
+  public addNewSpawn(
+    room: Room,
+    master: Item,
+    max: number,
+    ratePerSec: number
+  ) {
+    this._spawns.push({
+      room,
+      master,
+      maxQuantity: max,
+      elapsedTime: Math.floor(Math.random() * 500),
+      msToNextCheck: Math.floor(1000 / ratePerSec)
+    });
+  }
+
+  public checkSpawns(deltaT: number) {
+    this._spawns.forEach(data => {
+      data.elapsedTime += deltaT;
+      if (data.elapsedTime > data.msToNextCheck) {
+        data.elapsedTime -= data.msToNextCheck;
+        if (data.room.getItems().length < data.maxQuantity) {
+          this.spawn(data);
+        }
+      }
+    });
+  }
+
+  private spawn(data: Spawner.SpawnData) {
+    const controller = new Controller();
+    const item = data.master.copy(1);
+    controller.addItemsToRoom(data.room, [item]);
+    controller.sendUpdates();
+  }
+}
+
+namespace Spawner {
+  export interface SpawnData {
+    room: Room;
+    master: Item;
+    maxQuantity: number;
+    elapsedTime: number;
+    msToNextCheck: number;
+  }
+}
+
+export { Spawner };
