2bc89af9374b1b7fb73f2cbcb547821589c4588b
Mitchell Miller <mitchkm12@gmail.com>
2020-04-14 17:55:54 -0700

    ajdusts login screen look

--- client/states/login.ts
+++ client/states/login.ts
@@ -19,19 +19,19 @@ export class Login extends Phaser.State {
       height: inputFieldHeight
     });
 
-    const passwordField = this.game.add.inputField(this.game.world.centerX - 100, this.game.world.height / 4 + 16, {
-        type: PhaserInput.InputType.password,
-        placeHolder: "password",
-        font: "18px Space Mono",
-        width: inputFieldWidth,
-        height: inputFieldHeight
-    });
+    // const passwordField = this.game.add.inputField(this.game.world.centerX - 100, this.game.world.height / 4 + 16, {
+    //     type: PhaserInput.InputType.password,
+    //     placeHolder: "password",
+    //     font: "18px Space Mono",
+    //     width: inputFieldWidth,
+    //     height: inputFieldHeight
+    // });
 
-    const login = () => {ClientAPI.login(userField.value, passwordField.value)
+    const login = () => {ClientAPI.login(userField.value, "" /*passwordField.value*/)
       .then(res => {
           console.log("Success! " + ClientAPI.playerAgent);
           userField.destroy();
-          passwordField.destroy();
+          // passwordField.destroy();
           button.destroy();
           UI.instance.main.$data.activeQuests = ClientAPI.playerAgent.activeAssignedQuests.length;
           this.ready = true;
